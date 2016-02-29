a26bfc4804b68266916f497bc6fc11df4736cf0c
erynjones <eryn.jones16@hotmail.com>
2016-02-29 13:40:57 -0500

    making it work

--- js/main.js
+++ js/main.js
@@ -1,19 +1,28 @@
+var storyA = function () {
+  document.write('After snacking on ' + adjective + ' treats, the ' + colour + ' bellied ' + creature + verbed + ' for hours.');
+}
+
+var storyB = function () {
+  document.write('Ameilia ' + verbed + ' through the ' + adjective + colour + ' nebula escaping the space ' + creature + '.');
+}
+
+var storyC = function () {
+    document.write('Jackson chose his ' + adjective + ', ' + colour + creature + ' card and ' + verbed + ' it to the table knowing he won.');
+}
+
 var story = alert('Let’s make a story together!');
 var colour = prompt('A colour (orange, indigo, etc.)');
 var creature = prompt('A creature (dragon, moth, etc.)');
 var adjective = prompt('An adjective (beautiful, super, etc.)');
 var verbed = prompt('A past tense verb (ran, burped, etc.)');
+var story = prompt('Which story would you like?\na) Snacking\nb) Escaping\nc) Winning\nEnter the letter below');
 
-var group = prompt('Which story would you like?\na) Snacking\nb) Escaping\nc) Winning\nEnter the letter below');
-var abc = ['a, b, c'];
-
-if (group == 'A') {
-  // Story A
-  document.write(abc + 'After snacking on ' + adjective + ' treats, the ' + colour + ' bellied ' + creature + verbed + ' for hours.');
-} else if (group == 'B') {
-  // Story B
-  document.write(abc + 'Ameilia ' + verbed + ' through the ' + adjective + colour + ' nebula escaping the space ' + creature + '.');
-} else if (group == 'C') {
-  // Story C
-  document.write(abc + 'Jackson chose his ' + adjective + ', ' + colour + creature + ' card and ' + verbed + ' it to the table knowing he won.');
+if (story == 'a' || story == 'A') {
+  storyA();
+} else if (story == 'b' || story == 'B') {
+  storyB();
+} else if (story == 'c' || story == 'C') {
+  storyC();
+} else {
+  document.write('You are no fun.');
 }
